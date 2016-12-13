29dcb7040de3cfce911e7aabdde325d44fd7d6e3
Jean-Simon Desjardins <jsdesjardinssds@gmail.com>
2016-12-13 13:13:42 -0500

    Added System("PAUSE"); after kick line (fix #7 )
    Fixed #7

--- Program.cs
+++ Program.cs
@@ -52,6 +52,8 @@ namespace Magic8Ball
                 {
                     Console.ForegroundColor = ConsoleColor.Red;
                     Console.WriteLine("T'es encore plus poche ! BYE !");
+                    Console.ForegroundColor = ConsoleColor.White;
+                    System("PAUSE");
                     break;
                 }
 
